e794fe957c18e50daf1dd137aeae76577575c66d
Andrew Bae <kipumab@gmail.com>
2019-05-11 19:43:03 -0400

    Update

---
+++ Indecision-App/public/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+console.log('App.js is running');
+
+//JSX - Javascript XML
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ Indecision-App/src/app.js
@@ -0,0 +1,16 @@
+console.log('App.js is running');
+
+//JSX - Javascript XML
+var template =  (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
